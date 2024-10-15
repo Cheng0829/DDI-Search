2b9254f203abde8dff8e8b1e87c8eb968607ea9a
Cheng0829 <376023877@qq.com>
2024-10-15 10:05:55 +0800

    cjk

--- src/main/java/com/ddisearch/data/Qwen-API.py
+++ src/main/java/com/ddisearch/data/Qwen-API.py
@@ -75,8 +75,8 @@ if __name__ == "__main__":
             return "服务器繁忙，请稍后再试~"
         if(query_type == 1):
             # 查询已有DDI的原因
-            query = f"请简要回答（不要超过100个字）：为什么药物{drugA_name}和{drugB_name}之间存在如下联合作用：{ddi_description}？其中，两个药物的背景介绍如下：{drugA_description}，{drugB_description}."
+            query = f"请简要回答（不要超过100个字）：为什么药物{drugA_name}和{drugB_name}之间存在如下联合作用：{ddi_description}？其中，两个药物的背景介绍如下：{drugA_description}，{drugB_description}。输出格式为：药物{drugA_name}具有XXX的性质，而药物{drugB_name}具有YYY的性质，它们可能通过ZZZ的方式结合，进而导致这种药物相互作用。"
         else:
-            query = f"请简要回答（不要超过100个字）药物{drugA_name}和{drugB_name}之间可能存在哪些联合作用以及原因。其中，两个药物的背景介绍如下：{drugA_description}，{drugB_description}."
+            query = f"请简要回答（不要超过100个字）药物{drugA_name}和{drugB_name}之间可能存在哪些联合作用以及原因？其中，两个药物的背景介绍如下：{drugA_description}，{drugB_description}。药物{drugA_name}具有XXX的性质，而药物{drugB_name}具有YYY的性质，它们可能通过ZZZ的方式结合，进而产生XYZ的药物相互作用。"
         return llm(query)
     app.run(host='127.0.0.1', port=8290)
